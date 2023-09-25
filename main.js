475866bd8eb6ff7bc9febab9d3c4a543d5fb48c3
Jeremy <jectaunscripted@gmail.com>
2023-09-25 11:25:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+function genereerTafel() {
+  let tafelVan = parseInt(document.getElementById("tafelInput").value);
+
+  if (isNaN(tafelVan)) {
+    alert("Er is iets misgegaan bij het genereren van de tafels, heb je wel een cijfer ingevuld?");
+  } else {
+    let teller = 1
+    let uitkomst = 0
+    let tekst = ""
+
+    while (teller < 11) {
+      uitkomst = teller * tafelVan
+      tekst += tafelVan + "x" + teller + "=" + uitkomst + "<br>"
+      teller++
+    }
+    document.getElementById("tafel").innerHTML = tekst
+  }
+}
+
+function berekenPlus() {
+  let eersteInput = parseInt(document.getElementById("plus1").value);
+  let TweedeInput = parseInt(document.getElementById("plus2").value);
+
+  if (isNaN(eersteInput + TweedeInput)) {
+    alert("Er is iets misgegaan in de berekening, heb je de cijfers goed ingevuld?");
+  } else {
+    alert(eersteInput + TweedeInput);
+  }
+}
+
+function berekenMin() {
+  let eersteInput = parseInt(document.getElementById("-1").value);
+  let TweedeInput = parseInt(document.getElementById("-2").value);
+
+  if (isNaN(eersteInput - TweedeInput)) {
+    alert("Er is iets misgegaan in de berekening, heb je de cijfers goed ingevuld?");
+  } else {
+    alert(eersteInput - TweedeInput);
+  }
+}
+
+function berekenX() {
+  let eersteInput = parseInt(document.getElementById("x1").value);
+  let TweedeInput = parseInt(document.getElementById("x2").value);
+
+  if (isNaN(eersteInput * TweedeInput)) {
+    alert("Er is iets misgegaan in de berekening, heb je de cijfers goed ingevuld?");
+  } else {
+    alert(eersteInput * TweedeInput);
+  }
+}
+
+function berekenDelen() {
+  let eersteInput = parseInt(document.getElementById("deel1").value);
+  let TweedeInput = parseInt(document.getElementById("deel2").value);
+
+  if (isNaN(eersteInput / TweedeInput)) {
+    alert("Er is iets misgegaan in de berekening, heb je de cijfers goed ingevuld?");
+  } else {
+    alert(eersteInput / TweedeInput);
+  }
+}
+
+document.getElementById('btnSwitch').addEventListener('click', () => {
+  if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+    document.documentElement.setAttribute('data-bs-theme', 'light')
+  }
+  else {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+  }
+})
