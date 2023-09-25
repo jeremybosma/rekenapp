abe26200b729bf4c65a8bbcc7461386f9280248a
Jeremy <jectaunscripted@gmail.com>
2023-09-25 11:27:23 +0200

    thema slaat nu op in localstorage

--- main.js
+++ main.js
@@ -61,11 +61,20 @@ function berekenDelen() {
   }
 }
 
-document.getElementById('btnSwitch').addEventListener('click', () => {
+// bootstrap thema switcher
+const btnSwitch = document.getElementById('btnSwitch');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-bs-theme', currentTheme);
+}
+
+btnSwitch.addEventListener('click', () => {
   if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-    document.documentElement.setAttribute('data-bs-theme', 'light')
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+    localStorage.setItem('theme', 'light'); 
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); 
   }
-  else {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
-  }
-})
+});
