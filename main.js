5c5becf0dc46f543dd68316f86b6303fe7e7955c
Jeremy <jectaunscripted@gmail.com>
2023-09-26 11:12:34 +0200

    move "instellingen" to the right of the navbar.

--- main.js
+++ main.js
@@ -74,9 +74,9 @@ if (currentTheme) {
 btnSwitch.addEventListener('click', () => {
   if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
     document.documentElement.setAttribute('data-bs-theme', 'light');
-    localStorage.setItem('theme', 'light'); 
+    localStorage.setItem('theme', 'light');
   } else {
     document.documentElement.setAttribute('data-bs-theme', 'dark');
-    localStorage.setItem('theme', 'dark'); 
+    localStorage.setItem('theme', 'dark');
   }
 });
