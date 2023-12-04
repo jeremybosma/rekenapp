1a69192e0570c55490c38f5ff240e5a2d720338d
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-04 09:24:05 +0100

    Refactor unit selection and calculation logic

--- api/inc/components/eenheden.php
+++ api/inc/components/eenheden.php
@@ -3,31 +3,30 @@
         <div class="col-md-6 mt-2">
             <div class="card text-center">
                 <div id="tafel" class="card-body">
-                    <select class="form-select" style="font-weight: bold;">
-                        <option selected>Kies...</option>
-                        <option value="1">1D (lengte)</option>
-                        <option value="2">2D (Oppervlake)</option>
-                        <option value="3">3D (Inhoud)</option>
-                    </select>
+                <select id="inp_dim" class="form-select">
+              <option value="1">1D (lengte)</option>
+              <option value="2">2D (oppervlakte)</option>
+              <option value="3">3D (inhoud)</option>
+            </select>
 
                     <div class="row">
                         <div class="col-5">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control mt-3" placeholder="opgave" aria-describedby="basic-addon2" disabled>
-                                <span class="input-group-text mt-3" id="basic-addon2">mm</span>
+                                <input id="inp_left" type="text" class="form-control" placeholder="Opgave" disabled>
+                                <span id="eenh_left" class="input-group-text">mm</span>
                             </div>
                         </div>
                         <div class="col-1 mt-4">=</div>
                         <div class="col-6">
                             <div class="input-group mb-3">
-                                <input type="text" class="form-control mt-3" placeholder="antwoord" aria-describedby="basic-addon2">
-                                <span class="input-group-text mt-3" id="basic-addon2">km</span>
+                                <input id="inp_right" type="text" class="form-control" placeholder="Antwoord">
+                                <span id="eenh_right" class="input-group-text">km</span>
                             </div>
                         </div>
                     </div>
                     <div class="d-flex justify-content-between">
-                        <button class="btn btn-primary" onclick="genereerSom()">Genereer</button>
-                        <button class="btn btn-success" onclick="checkOplossing()">Check</button>
+                        <button class="btn btn-primary" onclick="generateProblem()">Genereer</button>
+                        <button class="btn btn-success" onclick="checkSolution()">Check</button>
                     </div>
                 </div>
             </div>
