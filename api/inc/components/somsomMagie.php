1177b616967a7ec693b21234482b5aa0b37e92f5
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-07 11:58:15 +0100

    Update somsomMagie.php and head.php files

--- api/inc/components/somsomMagie.php
+++ api/inc/components/somsomMagie.php
@@ -1,9 +1,33 @@
 <div class="row">
     <div class="col-md-6 mt-2">
-        <div id="alert-banner"></div>
         <div class="card text-center">
             <div id="tafel" class="card-body">
-                kaart
+                <div class="row">
+                    <div class="col">cel</div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col">cel</div>
+                </div>
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col">cel</div>
+                    <div class="col">cel</div>
+                    <div class="col">cel</div>
+                </div>
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col">cel</div>
+                    <div class="col">cel</div>
+                    <div class="col">cel</div>
+                </div>
+                <div class="row">
+                    <div class="col"></div>
+                    <div class="col">cel</div>
+                    <div class="col">cel</div>
+                    <div class="col"></div>
+                </div>
+                <br>
+                <button id="" class="btn btn-primary">Los SomSom op</button>
             </div>
         </div>
     </div>
