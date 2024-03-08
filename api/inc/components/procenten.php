b2d15135307ff004a78ee89db749646ecfe6b9cb
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-08 12:12:17 +0100

    Update procenten.php layout and add functionality

--- api/inc/components/procenten.php
+++ api/inc/components/procenten.php
@@ -1,24 +1,26 @@
-<div class="col-4">
+<div class="col-6">
     <div class="mb-3">
-        <select class="form-select is-invalid">
+        <select class="form-select is-invalid" id="select_soort">
             <option disabled selected>Kies*</option>
+            <option id="">van</option>
+            <option id="">toename</option>
+            <option id="">afname</option>
         </select>
     </div>
 
     <div class="input-group mb-3">
         <span class="input-group-text">Percentage:</span>
-        <input type="text" class="form-control is-invalid">
+        <input type="text" class="form-control is-invalid" id="inp_percentage">
         <span class="input-group-text">%</span>
     </div>
 
-    <div class="input-group mb-3">
-        <span class="input-group-text">Vermenigsvuldigingfactor: x</span>
-        <input type="text" class="form-control" disabled>
-        <span class="input-group-text">%</span>
+    <div class="input-group">
+        <span class="input-group-text">Vermenigsvuldigingfactor:</span>
+        <input type="text" class="form-control" id="inp_factor" disabled>
     </div>
 </div>
 
-<div class="row justify-content-between mb-3">
+<div class="row mb-3" style="margin-top: -180px;">
     <div class="col-3">
         <div class="card" style="margin-top: 200px">
             <div class="card-header">
@@ -34,12 +36,10 @@
                 </p>
             </div>
             <div class="card-footer text-body-secondary">
-                <input type="text" class="form-control is-invalid" id="oud" placeholder="Oud bedrag">
+                <input type="text" class="form-control is-invalid" id="inp_oud" placeholder="Oud bedrag">
             </div>
         </div>
     </div>
-
-    <img src="" alt="">
 
     <div class="col-3">
         <div class="card" style="margin-top: 200px">
@@ -56,24 +56,25 @@
                 </p>
             </div>
             <div class="card-footer text-body-secondary">
-                <input type="text" class="form-control is-invalid" id="oud" placeholder="Nieuw bedrag">
+                <input type="text" class="form-control is-invalid" id="inp_nieuw" placeholder="Nieuw bedrag">
             </div>
         </div>
     </div>
 </div>
 
-<div class="col-4">
+<div class="col-6">
     <div class="input-group mb-3">
-        <span class="input-group-text">Delingsfactor: :</span>
-        <input type="text" class="form-control" disabled>
-        <span class="input-group-text">%</span>
+        <span class="input-group-text">Deler:</span>
+        <input type="text" class="form-control" id="deler" disabled>
     </div>
 
     <div class="d-grid">
-        <button class="btn btn-success" onclick="losOp()" disabled>Los Op</button>
+        <button class="btn btn-success" onclick="losOp()">Los Op</button>
     </div>
 </div>
 
 <script>
-
+    function losOp() {
+        alert("Hij doet het, Ids is mijn absolute held");
+    }
 </script>
