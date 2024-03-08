e1092230912b14596b6e79b22c01e9895ea129bc
Jeremy Bosma <jectaunscripted@gmail.com>
2024-03-08 09:50:23 +0100

    Refactor form layout and add image placeholder

--- api/inc/components/procenten.php
+++ api/inc/components/procenten.php
@@ -1,27 +1,25 @@
-<div class="row">
-
-    <div class="col-4">
-        <div class="mb-3">
-            <select class="form-select is-invalid">
-                <option disabled selected>Kies*</option>
-            </select>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text">Percentage:</span>
-            <input type="text" class="form-control is-invalid">
-            <span class="input-group-text">%</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text">Vermenigsvuldigingfactor: x</span>
-            <input type="text" class="form-control" disabled>
-            <span class="input-group-text">%</span>
-        </div>
+<div class="col-4">
+    <div class="mb-3">
+        <select class="form-select is-invalid">
+            <option disabled selected>Kies*</option>
+        </select>
     </div>
 
-    <div class="col-3">
+    <div class="input-group mb-3">
+        <span class="input-group-text">Percentage:</span>
+        <input type="text" class="form-control is-invalid">
+        <span class="input-group-text">%</span>
+    </div>
 
+    <div class="input-group mb-3">
+        <span class="input-group-text">Vermenigsvuldigingfactor: x</span>
+        <input type="text" class="form-control" disabled>
+        <span class="input-group-text">%</span>
+    </div>
+</div>
+
+<div class="row justify-content-between mb-3">
+    <div class="col-3">
         <div class="card" style="margin-top: 200px">
             <div class="card-header">
                 Oud
@@ -39,15 +37,11 @@
                 <input type="text" class="form-control is-invalid" id="oud" placeholder="Oud bedrag">
             </div>
         </div>
-
     </div>
 
-    <div class="col-4">
-
-    </div>
+    <img src="" alt="">
 
     <div class="col-3">
-
         <div class="card" style="margin-top: 200px">
             <div class="card-header">
                 Nieuw
@@ -65,17 +59,21 @@
                 <input type="text" class="form-control is-invalid" id="oud" placeholder="Nieuw bedrag">
             </div>
         </div>
-
-        <div class="col-4">
-        <div class="input-group mb-3">
-            <span class="input-group-text">Delingsfactor: :</span>
-            <input type="text" class="form-control" disabled>
-            <span class="input-group-text">%</span>
-        </div>
-
-        <button class="btn btn-success" disabled>Los Op</button>
     </div>
-
-    </div>
-
 </div>
+
+<div class="col-4">
+    <div class="input-group mb-3">
+        <span class="input-group-text">Delingsfactor: :</span>
+        <input type="text" class="form-control" disabled>
+        <span class="input-group-text">%</span>
+    </div>
+
+    <div class="d-grid">
+        <button class="btn btn-success" onclick="losOp()" disabled>Los Op</button>
+    </div>
+</div>
+
+<script>
+
+</script>
