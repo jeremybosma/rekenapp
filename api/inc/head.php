3ca2513c333a033996b7cc2d4ffaf714395e65a2
Jeremy Bosma <jectaunscripted@gmail.com>
2024-04-12 12:17:07 +0200

    fix typo in head.php and remove favicon

--- api/inc/head.php
+++ api/inc/head.php
@@ -1,8 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Rekenapp - Delen</title>
+<title>Rekenapp</title>
 <meta name="description" content="App om rekenen te leren.">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<link rel="icon" href="inc/favicon.png" type="image/x-icon">
